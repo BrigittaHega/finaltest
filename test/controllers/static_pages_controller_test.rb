4eb853396e9756983274a11ff44a180b11618278
BrigittaHega <akuraegashiro@gmail.com>
2020-06-12 15:13:21 +1000

    add title and title test and more

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Clock+"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
+    assert_select "title", "Contact Us"
   end
 
 end
