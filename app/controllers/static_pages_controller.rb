1d54593dbd35b601a8c3cfd845ed2afff873870c
BrigittaHega <akuraegashiro@gmail.com>
2020-06-13 02:23:52 +1000

    Add time differences and day differences

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    if time.hour >= 9 && time.hour < 18
-    'business'
-    elsif time.hour >= 18 && time.hour < 20
-    'overtime'
-    elsif (time.hour >= 8 && time.hour < 9) || (time.hour >= 20 && time.hour < 23)
-    'person'
-    else
-    'sleeping'
-    end
+    
   end
 
   def contact
